ff1c037854ec64921aaaf08451bdfd5b0d47b604
Kole565 <rjkz565rjkz565@gmail.com>
2025-01-29 17:09:43 +0300

    Refactor and fix.

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from api.serializers import UserProfileSerializer
-from recipes.serializers import CustomRecipeSerializer
+from recipes.serializers import ShortRecipeSerializer
 from users.models import Subscription, User
 
 
-class SubscriptionSerializer(CustomUserSerializer):
+class SubscriptionSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField(
         read_only=True, method_name="get_recipes"
     )
@@ -25,6 +26,16 @@ class SubscriptionSerializer(CustomUserSerializer):
             "avatar",
         )
 
+    def get_is_subscribed(self, obj):
+        user = self.context.get("request").user
+
+        if not user.is_authenticated:
+            return False
+
+        return Subscription.objects.filter(
+            subscriber=user, author=obj.id
+        ).exists()
+
     def get_recipes(self, obj):
         request = self.context.get("request")
         recipes = obj.recipes.all()
@@ -36,6 +47,41 @@ class SubscriptionSerializer(CustomUserSerializer):
             except ValueError:
                 pass
 
-        return CustomRecipeSerializer(
+        return ShortRecipeSerializer(
             recipes, context={"request": request}, many=True
         ).data
+
+
+class CreateSubscriptionSerializer(serializers.ModelSerializer):
+    recipes = serializers.ReadOnlyField(source="author.recipes.all")
+    recipes_count = serializers.ReadOnlyField(source="author.recipes.count")
+
+    class Meta:
+        model = Subscription
+        fields = (
+            "author",
+            "subscriber",
+            "recipes",
+            "recipes_count",
+        )
+        extra_kwargs = {
+            "author": {'write_only': True},
+            "subscriber": {'write_only': True},
+        }
+
+    def to_representation(self, instance):
+        data = UserProfileSerializer(
+            instance.author, context={"request": self.context['request']}
+        ).data
+
+        data.update(super().to_representation(instance))
+
+        return data
+
+    def validate(self, data):
+        if data["subscriber"] == data["author"]:
+            raise serializers.ValidationError(
+                "Вы пытаетесь подписаться на себя!"
+            )
+
+        return data
