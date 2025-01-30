2d6f4c01517de915a5fee3a06fecc1a744c95d95
Kole565 <rjkz565rjkz565@gmail.com>
2025-01-30 20:52:24 +0300

    Refactor.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -26,15 +26,12 @@ class UserProfileSerializer(UserCreateSerializer):
     def get_is_subscribed(self, obj):
         user = self.context.get("request").user
 
-        if (
+        return (
             user.is_authenticated
             and user.subscriptions_where_subscriber.filter(
                 author=obj.id
             ).exists()
-        ):
-            return True
-
-        return False  # If we skip this line answer will not be boolean.
+        )
 
 
 class CreateUserProfileSerializer(UserProfileSerializer):
